b9d279f9aca44dd4ddf9b36c4b8088ef9f6fc08a
samiulbasirfahim <samiulbasirfahim@hotmail.com>
2022-04-28 02:11:48 +0600

    header responsive fixed. link added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { Route, Routes } from "react-router-dom"
 import Header from "./Components/Header"
 import useDarkMode from "./hooks/useDarkMode"
+import NotFound from "./Pages/NotFound"
 
 function App() {
 	const { isDarkMode, handleDarkMode } = useDarkMode()
 	return (
 		<div className={isDarkMode && "dark"}>
-			<div className="min-h-screen">
-        <Header></Header>
-      </div>
+			<div className="min-h-screen flex">
+				<Header handleDarkMode={handleDarkMode} isDarkMode={isDarkMode} ></Header>
+				<Routes>
+					<Route path={"*"} element={<NotFound></NotFound>}></Route>
+				</Routes>
+			</div>
 		</div>
 	)
 }
